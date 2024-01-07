b52e6452eb5788ba2e1ada9f2b918c3a799cc966
anasteiisha <anastasiyafrantseva5@gmail.com>
2024-01-07 23:15:28 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -52,20 +52,20 @@ export class App extends Component {
     );
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      console.log('update');
-
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
 
     if (parsedContacts) {
       this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('update');
+
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
 
